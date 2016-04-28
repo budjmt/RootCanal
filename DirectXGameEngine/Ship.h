148cd5bf8b8e07a9216f7628dc5f7388c42ea7bd
Ryan <rck1464@rit.edu>
2016-04-28 00:06:42 -0400

    Minor Changes
    Small updates to in-game objects

--- DirectXGameEngine/Ship.h
+++ DirectXGameEngine/Ship.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ColliderObject.h"
+#include "RootCanal.h"
 
 class Ship :
 	public ColliderObject
@@ -9,7 +10,10 @@ public:
 	Ship(Mesh* mesh, Material* material);
 	Ship(vec3 p, vec3 dims, vec3 sc, vec3 rA, float r, Drawable* s);
 
+	void addHealth(float hp);
+	void update();
 	void processMovement(float dt);
 private:
 	float health;
+	Camera* cam;
 };
